ce72c88b877576807e51b88a0c5363c1bffd6803
Rajdeep Ghatore <r.ghatore@gmail.com>
2021-03-02 15:24:29 -0700

    setup, and binding to custom properties using Input declarator

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  serverElements = [{type: 'server', name: 'test', content: 'hello, I\'m a server'}];
+}
